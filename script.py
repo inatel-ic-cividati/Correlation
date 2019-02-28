b0db22c04446b41c6fc6523f2b2ec1170ac347dd
Amor <rubens.cividati.teles@gmail.com>
2019-02-28 10:51:57 -0300

    Sqlite interaction
    added an interaction with sqlite

--- script.py
+++ script.py
@@ -4,26 +4,67 @@
 #   
 #   pip install beautifulsoup4
 
+import sqlite3
 from urllib.request import urlopen 
-
 from bs4 import BeautifulSoup as bs4
-#from datetime import datetime
+from datetime import datetime
+import time
+
+conn = sqlite3.connect('wow.db')
+cursor = conn.cursor()
+
 
 class data:
-    t = ''
+
+    date = ''
+    time = ''
     Us = ''
     Eu = ''
     Ch = ''
     Kr = ''
     Tw = ''
 
+def setDatabase(): 
+
+    cursor.execute("""
+    CREATE TABLE wowtoken(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        time TEXT NOT NULL,
+        Us TEXT NOT NULL,
+        Eu TEXT NOT NULL,
+        Ch TEXT NOT NULL,
+        Kr TEXT NOT NULL,
+        Tw TEXT NOT NULL
+    );
+    """)
+    print('Table created sucessfully')
+
+def insertDatabase(data):
+    try:
+        
+        values = data.date, data.time, data.Us ,data.Eu, data.Ch, data.Kr, data.Tw
+        cursor.execute("""
+        INSERT INTO wowtoken (date, time, Us, Eu, Ch, Kr, Tw)
+        VALUES """+str(values)+"""
+        """)
+        conn.commit()
+        print (data.time +' - Data inserted')
+
+    except Exception as e:
+        print('Problem in inserDatabase()!')
+        print('Exception: ', e)
+
 def getToken(data):
+
     url = 'https://wowtokenprices.com/'
     page = urlopen(url)
     soup = bs4(page, 'html.parser')
    
     aBox = soup.find_all('p', attrs={'class': 'money-text'})
-    #data.t = str(datetime.datetime.now().time())
+    
+    data.date = datetime.now().strftime("%m/%d/%Y")
+    data.time = datetime.now().strftime("%H:%M:%S")
     data.Us = aBox[0].text.strip() # North America
     data.Eu = aBox[1].text.strip() # Europe
     data.Ch = aBox[2].text.strip() # China
@@ -31,7 +72,9 @@ def getToken(data):
     data.Tw = aBox[4].text.strip() # Taiwan
     
 def showValues(data):
-    #print ('T :',data.t)
+
+    print ("time:", data.time)
+    print ("date:", data.date)
     print ("US:", data.Us)
     print ("Eu:", data.Eu)
     print ("Ch:", data.Ch)
@@ -39,14 +82,24 @@ def showValues(data):
     print ("Tw:", data.Tw)
 
 def fancyValues(data):
-    print ('|    T   |    Us   |    Eu   |    Ch   |    Kr   |    Tw   |')
-    print ('|',data.t,'|',data.Us,'|', data.Eu,'|', data.Ch,'|', data.Kr,'|', data.Tw,'|')
 
-while (True):
-    try:
-        print (data.t)
-        getToken(data)
-        showValues(data)
-        #fancyValues(data)
-    except:
-        print ('Error')
+    print ('|    Us   |    Eu   |    Ch   |    Kr   |    Tw   |')
+    print ('|',data.Us,'|', data.Eu,'|', data.Ch,'|', data.Kr,'|', data.Tw,'|')
+
+def main():
+
+    while (True):
+        try:
+            getToken(data)
+            insertDatabase(data)
+            fancyValues(data)
+            #sleep 20 minuts
+            #time.sleep(1800)
+            
+        except Exception as e:
+            print ('Problme in main()')
+            print ('Exception: ',e)
+            conn.close()
+            break
+
+main()
