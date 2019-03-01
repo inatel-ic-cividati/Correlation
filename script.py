5af789b3856238117e1c70c69170a741e3844b4a
Amor <rubens.cividati.teles@gmail.com>
2019-03-01 13:52:57 -0300

    Update script.py

--- script.py
+++ script.py
@@ -20,32 +20,6 @@ class data:
     Kr = ''
     Tw = ''
 
-def setDatabase(): 
-
-    cursor.execute("""
-    CREATE TABLE wowtoken(
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        date TEXT NOT NULL,
-        time TEXT NOT NULL,
-        Us TEXT NOT NULL,
-        Eu TEXT NOT NULL,
-        Ch TEXT NOT NULL,
-        Kr TEXT NOT NULL,
-        Tw TEXT NOT NULL
-    );
-    """)
-    print('Table wowtoken created sucessfully!')
-
-    cursor.execute("""
-    CREATE TABLE log(
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        date TEXT NOT NULL,
-        time TEXT NOT NULL,
-        message TEXT
-    );
-    """)
-    print ('Table log created sucessfully!')
-
 def insertData(data):
 
     try:  
@@ -108,8 +82,6 @@ def fancyValues(data):
     print ('|',data.Us,'|', data.Eu,'|', data.Ch,'|', data.Kr,'|', data.Tw,'|')
 
 def main():
-
-    setDatabase()
 
     while (True):
         try:
