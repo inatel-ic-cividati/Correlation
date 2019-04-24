eafe772dfd22e2bf9b3b9a8136f5d2b1f47fbb78
Amor <rubens.cividati.teles@gmail.com>
2019-04-24 16:36:30 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,12 +3,10 @@ import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime
 sns.set(style="darkgrid")
  
-conn = sqlite3.connect('wow.db')
-query = 'SELECT * FROM wowtoken WHERE id <= 20 and Us != "";'
-df = pd.read_sql_query(query,conn)
-df = df(index = df['date'])
+
 
 def convertStrFlt(dataFrame, name):
     
@@ -17,17 +15,48 @@ def convertStrFlt(dataFrame, name):
     dataFrame[name] = dataFrame[name].astype('float')
     df = dataFrame
 
-convertStrFlt(df, 'Us')
-convertStrFlt(df, 'Eu')
-convertStrFlt(df, 'Ch')
-convertStrFlt(df, 'Tw')
-convertStrFlt(df, 'Kr')
+def convertStrDate(dataFrame):
 
-print (df)
-fmri = sns.load_dataset("fmri")
-#print(fmri)
-#sns.lineplot(data=fmri,palette="tab10",linewidth=2.5)
-#g = sns.regplot(x="date", y="Us", data=df)
-#g = sns.relplot(x="timepoint", y="signal", hue="event", style="event", col="region",kind="line", data=fmri)
-g.fig.autofmt_xdate()
-plt.show()
+    # converting the column date to datetime64
+    dataFrame['date'] = dataFrame['date'].str.replace('/','-')
+    
+    for i in dataFrame['date']:
+        aStr = i.split('-')
+        aD = aStr[1]
+        aM = aStr[0]
+        aY = aStr[2]
+        i = aY +'-'+ aM + '-' + aD
+        dataFrame['date'] = i
+        pass
+
+    dataFrame['date'] = dataFrame['date']+' '+dataFrame['time']
+    dataFrame['date'] = dataFrame['date'].astype('datetime64[s]')
+
+    dataFrame.index = dataFrame['date']
+    dataFrame = dataFrame.drop(columns='time')
+    dataFrame = dataFrame.drop(columns='id')
+    dataFrame = dataFrame.drop(columns='date')
+    df = dataFrame
+
+def main():
+
+    conn = sqlite3.connect('wow.db')
+    query = 'SELECT * FROM wowtoken WHERE id <= 20 and Us != "";'
+    df = pd.read_sql_query(query,conn)
+
+    convertStrFlt(df, 'Us')
+    convertStrFlt(df, 'Eu')
+    convertStrFlt(df, 'Ch')
+    convertStrFlt(df, 'Tw')
+    convertStrFlt(df, 'Kr')
+    convertStrDate(df)
+    df = df.drop(columns='time')
+    df = df.drop(columns='id')
+    df = df.drop(columns='date')
+    
+    #print (df)
+    
+    g = sns.lineplot(data=df,palette='tab10',linewidth=2.5)
+
+
+main()
