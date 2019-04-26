3954fe624cbc0a8b5c229407aaaded5c1020cbde
Amor <rubens.cividati.teles@gmail.com>
2019-04-26 13:48:08 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
-sns.set(style="darkgrid")
+sns.set_palette("husl")
  
 def convertStrFlt(dataFrame, name):
     
@@ -17,29 +17,16 @@ def convertStrDate(dataFrame):
 
     # converting the column date to datetime64
     dataFrame['date'] = dataFrame['date'].str.replace('/','-')
-    
-    for i in dataFrame['date']:
-        aStr = i.split('-')
-        aD = aStr[1]
-        aM = aStr[0]
-        aY = aStr[2]
-        i = aY +'-'+ aM + '-' + aD
-        dataFrame['date'] = i
-        pass
-
     dataFrame['date'] = dataFrame['date']+' '+dataFrame['time']
     dataFrame['date'] = dataFrame['date'].astype('datetime64[s]')
-
     dataFrame.index = dataFrame['date']
-    dataFrame = dataFrame.drop(columns='time')
-    dataFrame = dataFrame.drop(columns='id')
-    dataFrame = dataFrame.drop(columns='date')
+
     df = dataFrame
 
 def main():
 
     conn = sqlite3.connect('wow.db')
-    query = 'SELECT * FROM wowtoken WHERE Us != "";'
+    query = 'SELECT * FROM wowtoken WHERE id <= 50 and Us != "";'
     df = pd.read_sql_query(query,conn)
 
     convertStrFlt(df, 'Us')
@@ -48,12 +35,11 @@ def main():
     convertStrFlt(df, 'Tw')
     convertStrFlt(df, 'Kr')
     convertStrDate(df)
-
-    df = df.drop(['time', 'date', 'id'], axis = 1)
+    #df = df.drop(['time','id'], axis = 1)
     
-    print (df)
-    
-    g = sns.lineplot(data=df,palette='tab10',linewidth=2.5)
+    g1 = sns.regplot(x='date',y='Us',data=df, fit_reg=True)
+    g1.set(xlabel='time', ylabel='value',title='Wow token value')
     plt.show()
-
+    
+    
 main()
