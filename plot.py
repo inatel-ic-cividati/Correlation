f29d989d7a6a10e6683c1d0bf03a647f74cf0e01
Amor <rubens.cividati.teles@gmail.com>
2019-05-21 10:40:50 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,44 +2,100 @@ import pandas as pd
 import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime
-sns.set_palette("husl")
+plt.style.use('dark_background')
  
 def convertStrFlt(dataFrame, name):
     
     # converting str to float
     dataFrame[name] = dataFrame[name].str.replace(',','.')
     dataFrame[name] = dataFrame[name].astype('float')
-    df = dataFrame
+    
+    return dataFrame
 
 def convertStrDate(dataFrame):
 
     # converting the column date to datetime64
     dataFrame['date'] = dataFrame['date'].str.replace('/','-')
     dataFrame['date'] = dataFrame['date']+' '+dataFrame['time']
-    dataFrame['date'] = dataFrame['date'].astype('datetime64[s]')
+    dataFrame['date'] = dataFrame['date'].astype('datetime64[m]')
     dataFrame.index = dataFrame['date']
 
-    df = dataFrame
+    return dataFrame
 
 def main():
 
     conn = sqlite3.connect('wow.db')
-    query = 'SELECT * FROM wowtoken WHERE id <= 50 and Us != "";'
-    df = pd.read_sql_query(query,conn)
+    cursor = conn.cursor()
 
-    convertStrFlt(df, 'Us')
-    convertStrFlt(df, 'Eu')
-    convertStrFlt(df, 'Ch')
-    convertStrFlt(df, 'Tw')
-    convertStrFlt(df, 'Kr')
-    convertStrDate(df)
-    #df = df.drop(['time','id'], axis = 1)
+    # SETTING UP WOWTOKEN DATA FRAME
+    query = 'SELECT * FROM wowtoken WHERE Us != "";'
+    dfWowtoken = pd.read_sql_query(query,conn)
     
-    g1 = sns.regplot(x='date',y='Us',data=df, fit_reg=True)
-    g1.set(xlabel='time', ylabel='value',title='Wow token value')
-    plt.show()
-    
+    dfWowtoken = convertStrFlt(dfWowtoken, 'Us')
+    dfWowtoken = convertStrFlt(dfWowtoken, 'Eu')
+    dfWowtoken = convertStrFlt(dfWowtoken, 'Ch')
+    dfWowtoken = convertStrFlt(dfWowtoken, 'Tw')
+    dfWowtoken = convertStrFlt(dfWowtoken, 'Kr')
+    dfWowtoken = convertStrDate(dfWowtoken)
+    dfWowtoken = dfWowtoken.drop(['time','id','date'], axis = 1)
+
+    # SETTING UP CURRENCY DATA FRAME
+    query = 'UPDATE currency SET Brl="0" WHERE Brl IS NULL;'
+    cursor.execute(query)
+
+    query = 'SELECT * FROM currency;'
+    dfCurrency = pd.read_sql_query(query,conn)
+
+    convertStrFlt(dfCurrency,'Usd')
+    convertStrFlt(dfCurrency,'Eur')
+    convertStrFlt(dfCurrency,'Cny')
+    convertStrFlt(dfCurrency,'Krw')
+    convertStrFlt(dfCurrency,'Brl')
+    convertStrDate(dfCurrency)
+
+    print(dfCurrency)
+    dfCurrency = dfCurrency.drop(['time','id','date'], axis = 1)
+
+    # PARAMS
+    yColor = '#00FFFF' # #00FFFF -> BLUE
+    xColor = '#FF1493' # #FF1493 -> PINK
+    xRotation = 20
+
+    op = 1
+
+    if op == 0:
+        country = 'Us'
+        currency = 'Usd'
+    elif op == 1:
+        country = 'Eu'
+        currency = 'Eur'
+    elif op == 2:
+        country = 'Ch'
+        currency = 'Cny'
+    elif op == 3:
+        country = 'Kr'
+        currency = 'Krw'
+    elif op == 4:
+        country = 'Eu'
+        currency = 'Brl'
+
+    plt.subplot(2,1,1)
+    plt.plot(dfWowtoken[country], label=country, color = yColor)
+    plt.xlabel('Time', color = xColor)
+    plt.xticks(rotation = xRotation, color = xColor)
+    plt.yticks(color = yColor)
+    plt.ylabel('Value', color = yColor)
+    plt.legend()
+
+    plt.subplot(2,1,2)
+    plt.plot(dfCurrency[currency], label=currency, color = yColor)
+    plt.xlabel('Time', color = xColor)
+    plt.xticks(rotation = xRotation, color = xColor)
+    plt.yticks(color = yColor)
+    plt.ylabel('Value', color = yColor)
+    plt.legend()
+  
+    plt.show()    
     
 main()
