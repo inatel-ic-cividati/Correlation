a0813119b08d6f1b6149868e9d93c2368d36362c
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-09-19 16:17:36 -0300

    Update setDatabase.py

--- setDatabase.py
+++ setDatabase.py
@@ -1,49 +1,34 @@
 import sqlite3
+import os
+from data import database as db
+
+def set_Table(tableName): 
+    try:
+        # creating wowtoken table
+        colunmName = tableName.replace("wowtoken_","")
+        colunmName = colunmName.replace("currency_","")
+
+        cursor.execute("""
+        CREATE TABLE """+tableName+"""(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            time INT NOT NULL,
+            """+colunmName+""" FLOAT NOT NULL
+        );
+        """)
+        print(tableName+' table created sucessfully!')
+
+    except sqlite3.Error as e:
+            print(tableName + " error: ", e)
 
 # creating the databse 
-db_url = 'C:/YOUR/FOLDER/HERE/'
-conn = sqlite3.connect(db_url+'wow.db')
+conn = db.get_connection('wow.db')
 cursor = conn.cursor()
 print('wow.db created sucessfully!')
 
-def setDatabase(): 
-    try:
-        # creating wowtoken table
-        cursor.execute("""
-        CREATE TABLE wowtoken(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            date TEXT NOT NULL,
-            time TEXT NOT NULL,
-            Us TEXT NOT NULL,
-            Eu TEXT NOT NULL,
-            Ch TEXT NOT NULL,
-            Kr TEXT NOT NULL,
-            Tw TEXT NOT NULL
-        );
-        """)
-        print('wowtoken table created sucessfully!')
-    except sqlite3.Error as e:
-            print("Database error: "+ e)
+params = ['us', 'eu', 'china', 'korea', 'taiwan']
 
-    try:
-        # creating currency table
-        cursor.execute("""
-        CREATE TABLE currency(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            date TEXT NOT NULL,
-            time TEXT NOT NULL,
-            Usd TEXT NOT NULL,
-            Eur TEXT NOT NULL,
-            Cny TEXT NOT NULL,
-            Krw TEXT NOT NULL,
-            Brl TEXT NOT NULL
-        );
-        """)
-
-        print('currency table created sucessfully!')
-    except sqlite3.Error as e:
-            print("Database error: "+ e)
-
-setDatabase()
+for table in params:
+    set_Table('wowtoken_'+table)
+    set_Table('currency_'+table)
 
 conn.close()
