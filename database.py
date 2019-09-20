18fc962fa9ac9fcadb49e441db56644d236720d4
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-09-20 17:53:36 -0300

    Update database.py

--- database.py
+++ database.py
@@ -8,51 +8,50 @@ import pandas as pd
 
 def set_DataBase():
     # creating the databse 
-    conn = db.get_connection('wow.db')
-    cursor = conn.cursor()
-    print('wow.db created sucessfully!')
-
-    params = ['us', 'eu', 'china', 'korea', 'taiwan']
-
-    for table in params:
-        set_Table('wowtoken_'+table, cursor)
-        set_Table('currency_'+table, cursor)
-
-    conn.close()
-
-def set_Table(tablewowtoken, cursor): 
     try:
-        # creating wowtoken table
-        colunmwowtoken = tablewowtoken.replace("wowtoken_","")
-        colunmwowtoken = colunmwowtoken.replace("currency_","")
+        conn = db.get_connection('wow.db')
+        cursor = conn.cursor()
 
-        cursor.execute("""
-        CREATE TABLE """+tablewowtoken+"""(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            time INT NOT NULL,
-            """+colunmwowtoken+""" FLOAT NOT NULL
-        );
-        """)
-        print(tablewowtoken+' table created sucessfully!')
+        params = ['us', 'eu', 'china', 'korea', 'taiwan']
+
+        for table in params:
+            set_Table('wowtoken_'+table, cursor)
+            set_Table('currency_'+table, cursor)
+
+        conn.close()
 
     except sqlite3.Error as e:
-            print(tablewowtoken + " error: ", e)
+        print('error:',e)
 
-def insert_Table(tuple, tablewowtoken):
+def set_Table(wowtokentable, cursor): 
+    try:
+        # creating wowtoken table
+        wowtokencolunm = wowtokentable.replace("wowtoken_","")
+        wowtokencolunm = wowtokencolunm.replace("currency_","")
 
-    print('a')
+        cursor.execute("""
+        CREATE TABLE """+wowtokentable+"""(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            time INT NOT NULL,
+            """+wowtokencolunm+""" FLOAT NOT NULL
+        );
+        """)
+        print(wowtokentable+' table created sucessfully!')
+
+    except sqlite3.Error as e:
+            print(wowtokentable + " error: ", e)
+
+def insert_Table(df, wowtokentable):
+    # unfinished
 
 
 def get_Data(wowtoken):
-    #unfinished
-    js = requests.get('https://wowtokenprices.com/history_prices_1_day.json')
-    js = js.json()
-
-    data = pd.DataFrame(columns={'us','eu'})
-
-    for index in js['us']:
-        print(index)
-
-    print(data)
+    # implements to get all tokens
+    req = requests.get('https://wowtokenprices.com/history_prices_full.json')
+    resp = req.text
+    json_obj = json.loads(resp)
+    data = pd.DataFrame(json_obj['us'])
+    
+    return data
 
 get_Data('us')
