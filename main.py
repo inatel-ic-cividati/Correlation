29f37c304122d3511fbca29534feedb8cc3341ab
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-09-20 18:13:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import database
 
-database.set_DataBase()
-data = database.get_Data('us')
+#database.set_DataBase()
+dataUs = database.get_Data('us')
+dataEu = database.get_Data('eu')
+dataChina = database.get_Data('china')
+dataKorea = database.get_Data('korea')
+dataTaiwan = database.get_Data('taiwan')
 
-print(data)
+database.insert_Table(dataUs, 'wowtoken_us')
+database.insert_Table(dataEu, 'wowtoken_eu')
+database.insert_Table(dataChina, 'wowtoken_china')
+database.insert_Table(dataKorea, 'wowtoken_korea')
+database.insert_Table(dataTaiwan, 'wowtoken_taiwan')
+
+#print(dataUs)
