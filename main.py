0f5ed3a40afba76c35c7de4c86f3157cee7e9520
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-09-24 20:52:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import database
+import pandas as pd
 
-#database.set_DataBase()
+# create the database with the tables
+database.set_DataBase()
+
+# get the data
 dataUs = database.get_Data('us')
 dataEu = database.get_Data('eu')
 dataChina = database.get_Data('china')
 dataKorea = database.get_Data('korea')
 dataTaiwan = database.get_Data('taiwan')
+print()
+data = [dataUs, dataEu, dataChina, dataKorea, dataTaiwan]
 
+for i in data:
+    i = database.unix_datetime(i)
+    i = database.set_Avg_Field(i)
+    i = database.set_Normalized_Field(i)
+
+# insert into database
 database.insert_Table(dataUs, 'wowtoken_us')
 database.insert_Table(dataEu, 'wowtoken_eu')
 database.insert_Table(dataChina, 'wowtoken_china')
 database.insert_Table(dataKorea, 'wowtoken_korea')
 database.insert_Table(dataTaiwan, 'wowtoken_taiwan')
-
-#print(dataUs)
