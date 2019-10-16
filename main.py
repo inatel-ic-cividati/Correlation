c0191bb37b9c3da4e8edd0e3afc28b5f79c32ce0
Rubens Cividati <rubens.cividati.teles@gmail.com>
2019-10-16 16:12:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-import database
+import database # personal script
+import analytics #personal script
+
+import view
 import pandas as pd
 import os
+from data import database as db
 
-# create the database with the tables
-database.set_DataBase()
-os.system('cls')
-
-# get the data
-dataUs = database.get_Data('us')
-dataEu = database.get_Data('eu')
-dataChina = database.get_Data('china')
-dataKorea = database.get_Data('korea')
-dataTaiwan = database.get_Data('taiwan')
-os.system('cls')
-
-data = [dataUs, dataEu, dataChina, dataKorea, dataTaiwan]
-
-for i in data:
-    i = database.unix_datetime(i)
-    i = database.set_Avg_Field(i)
-    i = database.set_Normalized_Field(i)
-    i = i.reindex(sorted(i.columns), axis = 1)
+def main_not_finished():
+    # create the database with the tables
+    database.set_DataBase()
     os.system('cls')
 
-# insert into database
-database.insert_Table(dataUs, 'wowtoken_us')
-database.insert_Table(dataEu, 'wowtoken_eu')
-database.insert_Table(dataChina, 'wowtoken_china')
-database.insert_Table(dataKorea, 'wowtoken_korea')
-database.insert_Table(dataTaiwan, 'wowtoken_taiwan')
-os.system('cls')
+    # get the data
+    dataUs = database.get_Data('us')
+    dataEu = database.get_Data('eu')
+    dataChina = database.get_Data('china')
+    dataKorea = database.get_Data('korea')
+    dataTaiwan = database.get_Data('taiwan')
+    os.system('cls')
 
-print('Script done, your data is sotored in /data/wow.db!')
+    data = [dataUs, dataEu, dataChina, dataKorea, dataTaiwan]
+
+    for i in data:
+        i = database.unix_datetime(i)
+        i = database.set_Avg_Field(i)
+        i = database.set_Normalized_Field(i)
+        i = i.reindex(sorted(i.columns), axis = 1)
+
+    # insert into database
+    database.insert_Table(dataUs, 'wowtoken_us')
+    database.insert_Table(dataEu, 'wowtoken_eu')
+    database.insert_Table(dataChina, 'wowtoken_china')
+    database.insert_Table(dataKorea, 'wowtoken_korea')
+    database.insert_Table(dataTaiwan, 'wowtoken_taiwan')
+    os.system('cls')
+
+    print('Your data is sotored in /data/wow.db!')
+    print(dataEu['price'])
+
+def main(): 
+
+    dfWowtoken = database.read_Data('wowtoken')
+    dfCurrency = database.read_Data('currency')
+
+    df = database.join_data(dfCurrency, dfWowtoken)
+    data = df['Us']
+
+main()
+
+data = [0,1,2,3,4,5,6,7,8,9]
+print(analytics.autoregressive(data, 2))
+print(analytics.arima(data, 2))
